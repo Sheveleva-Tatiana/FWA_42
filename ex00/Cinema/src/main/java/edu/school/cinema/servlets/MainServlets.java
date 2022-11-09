425957c85a830864c07e96eece243846b2d39ba2
sshera <tanya.myau@mail.ru>
2022-11-09 09:42:20 +0300

    Научилась сохранять инфомацию в сессию

--- ex00/Cinema/src/main/java/edu/school/cinema/servlets/MainServlets.java
+++ ex00/Cinema/src/main/java/edu/school/cinema/servlets/MainServlets.java
@@ -1,5 +1,7 @@
 package edu.school.cinema.servlets;
 
+import edu.school.cinema.test.Cart;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,15 +17,38 @@ public class MainServlets extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            String name = req.getParameter("name");
-            String surname = req.getParameter("surname");
-            PrintWriter pw = resp.getWriter();
-            pw.println("<html>");
-            pw.println("<h1> Hello, " + name + " " +    surname + "! </h1>");
-            pw.println("</html>");
+        HttpSession session = req.getSession();
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
-        dispatcher.forward(req, resp);
+//        String user = (String) session.getAttribute("current_user");
+//        if (user == null) {
+//            // resp для анонимного пользователя
+//            // авторизация/регистрация пользователя
+//            // session.setAttribute("current_user", ID);
+//        } else {
+//            // resp для авторизованного пользователя
+//        }
+
+        Cart cart = (Cart) session.getAttribute("cart");
+        String name = req.getParameter("name");
+        Integer quantity = Integer.parseInt(req.getParameter("quantity"));
+        if (cart == null) {
+          cart = new Cart(name, quantity);
+        }
+        session.setAttribute("cart", cart);
+
+
+        getServletContext().getRequestDispatcher("/showCart.jsp").forward(req, resp);
+
+//        String name = req.getParameter("name");
+//        String surname = req.getParameter("surname");
+
+//        PrintWriter pw = resp.getWriter();
+//        pw.println("<html>");
+//        pw.println("<h1> Your count is  "  + "! </h1>");
+//        pw.println("</html>");
+
+//        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
+//        dispatcher.forward(req, resp);
     }
 
 }
